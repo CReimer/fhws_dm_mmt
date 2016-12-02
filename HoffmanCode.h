691cb89086482622205809aa2091faafe90f38eb
Christopher Reimer <mail+gitproject@c-reimer.de>
2016-12-02 13:21:10 +0100

    HoffmanCode Klasse an Coding Style des restlichen Codes angepasst.

--- HoffmanCode.h
+++ HoffmanCode.h
@@ -1,76 +1,76 @@
 #pragma once
+
 #include <vector>
 #include <iostream>
 #include <string>
-using namespace::std;
 
+using namespace std;
 
-class HoffmanCode
-{
+class HoffmanCode {
 public:
-	HoffmanCode();
-	~HoffmanCode();
+    HoffmanCode();
 
-	vector<int> freqTree; // Häufigkeitsbaum; eigentlich kein vector, sondern entsprechende Baumstruktur
-	vector<vector<char>> codeTable; //Codetabelle für spätere Zuordnung char -> binär, vector weil unbestimmt lang
+    ~HoffmanCode();
 
-	//	3a)
-	//zählt Häufigkeiten jedes chars und gibt es als array zurück
-	//fügt in Klassenvariable codeTable die chars distinct ein
-	int* charCount(char* sentence);
+    vector<int> freqTree; // HÃ¤ufigkeitsbaum; eigentlich kein vector, sondern entsprechende Baumstruktur
+    vector<vector<char>> codeTable; //Codetabelle fÃ¼r spÃ¤tere Zuordnung char -> binÃ¤r, vector weil unbestimmt lang
 
-	//bekommt int array mit den Häufigkeitswerten aller chars
-	//setzt jeweils die zwei kleinsten Knoten zu Zweig zusammen, bis Baum übrig bleibt
-	//Baumstruktur als Klassenvariable... geeignete Struktur vorhanden? int Liste?
-	void setTree(int* countArray);
+    //	3a)
+    //zÃ¤hlt HÃ¤ufigkeiten jedes chars und gibt es als array zurÃ¼ck
+    //fÃ¼gt in Klassenvariable codeTable die chars distinct ein
+    int *charCount(char *sentence);
 
-	//bekommen nichts übergeben (oder Baum, wenn Baum keine Klassenvariable)
-	//codiert jeden Weg binär (links 0, rechts 1)
-	//erstellt Code für jedes Blatt, indem es die Bits des jeweiligen Wegs zusammenhängt
-	//gibt Code aus (vector<bool> oder die map?)
-	//hier muss noch eine Zuordnung char -> Binärcode stattfinden  (wie?)
-	vector<bool> codeTree();
+    //bekommt int array mit den HÃ¤ufigkeitswerten aller chars
+    //setzt jeweils die zwei kleinsten Knoten zu Zweig zusammen, bis Baum Ã¼brig bleibt
+    //Baumstruktur als Klassenvariable... geeignete Struktur vorhanden? int Liste?
+    void setTree(int *countArray);
 
-	//bekommt eine Binärkette übergeben und die Zuordnungstabelle char -> binärcode
-	//geht Binärkette durch und sucht längst möglichen Binärcode, der auch in der Tabelle ist (es gibt keine codes mit gleichen zwei Bits am Anfang)
-	//schreibt entsprechenden char und wdh. bis Kette leer
-	//gibt String aus
-	char* decodeTree(vector<bool> binaryChain, vector<vector<char>> codeTable);
+    //bekommen nichts Ã¼bergeben (oder Baum, wenn Baum keine Klassenvariable)
+    //codiert jeden Weg binÃ¤r (links 0, rechts 1)
+    //erstellt Code fÃ¼r jedes Blatt, indem es die Bits des jeweiligen Wegs zusammenhÃ¤ngt
+    //gibt Code aus (vector<bool> oder die map?)
+    //hier muss noch eine Zuordnung char -> BinÃ¤rcode stattfinden  (wie?)
+    vector<bool> codeTree();
 
-	//-------------------------------------------
+    //bekommt eine BinÃ¤rkette Ã¼bergeben und die Zuordnungstabelle char -> binÃ¤rcode
+    //geht BinÃ¤rkette durch und sucht lÃ¤ngst mÃ¶glichen BinÃ¤rcode, der auch in der Tabelle ist (es gibt keine codes mit gleichen zwei Bits am Anfang)
+    //schreibt entsprechenden char und wdh. bis Kette leer
+    //gibt String aus
+    char *decodeTree(vector<bool> binaryChain, vector<vector<char>> codeTable);
 
-	//	3b)
-	//anstatt die zwei kleinsten Knoten zusammen zu setzen:
-	//neuen Knoten k(i) mit vorhandenen Knoten vergleichen
-	//k(i-1) < k(i) => ja? k(i-1) zu rechtem Knoten von k(i) machen; nein?: 
-	//checken, ob k(i+1) vorhanden. ja?:
-	//k(i-1) > k(i) < k(i+1) => k(i) zu rechtem Knoten von k(i+1) machen
-	//k(i-1) > k(i) > k(i+1) => k(i) zu rechtem Knoten von k(i-1) machen und k(i+1) zu rechtem Knoten von k(i)
-	//nein?:
-	//k(i-1) > k(i) => k(i) zu rechtem Knoten von k(i+1) machen
-	void setTree2(int* countArray);
+    //-------------------------------------------
 
-	//--------------------------------------------
+    //	3b)
+    //anstatt die zwei kleinsten Knoten zusammen zu setzen:
+    //neuen Knoten k(i) mit vorhandenen Knoten vergleichen
+    //k(i-1) < k(i) => ja? k(i-1) zu rechtem Knoten von k(i) machen; nein?:
+    //checken, ob k(i+1) vorhanden. ja?:
+    //k(i-1) > k(i) < k(i+1) => k(i) zu rechtem Knoten von k(i+1) machen
+    //k(i-1) > k(i) > k(i+1) => k(i) zu rechtem Knoten von k(i-1) machen und k(i+1) zu rechtem Knoten von k(i)
+    //nein?:
+    //k(i-1) > k(i) => k(i) zu rechtem Knoten von k(i+1) machen
+    void setTree2(int *countArray);
 
-	//3c)
-	//Problemstellung: keine reinen 1-Codes
-	//checken: wenn letzter rechter Knoten erreicht, muss Blatt links angehängt werden statt rechts
-	//--> ganz rechtes Blatt des Baumes muss leer sein
-	//sonst wie oben
-	void codeTree3(int* countArray);
+    //--------------------------------------------
 
-	//--------------------------------------------
+    //3c)
+    //Problemstellung: keine reinen 1-Codes
+    //checken: wenn letzter rechter Knoten erreicht, muss Blatt links angehÃ¤ngt werden statt rechts
+    //--> ganz rechtes Blatt des Baumes muss leer sein
+    //sonst wie oben
+    void codeTree3(int *countArray);
 
-	//	3d)
-	//bekommt maximale Tiefe übergeben
-	//hört auf, Äste aneinander zu setzen, wenn depth Knoten erreicht sind
-	//Problem: Was passiert, wenn mehr Knoten übrig sind??
-	void setTree4(int* countArray, int depth);
+    //--------------------------------------------
 
-	//--------------------------------------------
+    //	3d)
+    //bekommt maximale Tiefe Ã¼bergeben
+    //hÃ¶rt auf, Ã„ste aneinander zu setzen, wenn depth Knoten erreicht sind
+    //Problem: Was passiert, wenn mehr Knoten Ã¼brig sind??
+    void setTree4(int *countArray, int depth);
 
-	//	3e)
-	//
+    //--------------------------------------------
+
+    //	3e)
+    //
 
 };
-
